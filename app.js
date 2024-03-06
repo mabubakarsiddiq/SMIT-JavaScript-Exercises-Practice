162a3f3394acc22c1ae8398a6480849da7587980
muhammadabubakarsiddiqkhan <muhammadabubakarsiddiqkhan111@gmail.com>
2024-03-07 00:34:49 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,84 @@
+ 
+                                                                    // Chapter 01 (Alert)
+ // Q 1
+    
+    // alert("")
+
+// Q2
+
+    // alert("Erroe! Please enter a valid password")
+
+// Q3
+
+    // alert("Welcome to JS land... \n Happy Coding")
+
+// Q4
+
+    //  alert("Welcome to JS land...")
+    //  alert("Happy Coding! \n prevent this page from creating additional dialog.")
+
+
+                                                                    // CHAPTER 02 (Variable For String)
+
+// Q 1
+
+    // var username = ("Username")
+
+// Q 2  
+
+    // var fullname = ("Md Abu Bakar Siddiq Khan")
+
+// Q 3
+
+    // var message = ("Hello World!")
+    // alert (message)
+
+// Q 4    
+
+    // var name = ("SAIN")
+    // alert(name)
+
+    // var age = ("Age 21")
+    // alert(age)
+
+    // var skills =("Web And Mobile Development")
+    // alert(skills)
+
+// Q 5
+
+    // var pizza = ("pizza") 3rd
+
+    // pizza = ("pizz") 2nd
+
+    // pizza = ("piz") 1st
+
+    // alert(pizza)
+
+
+    // Q 6
+
+        // document.write("<h1>Yah! I Can Write HTML Content Through JavaScript</h1> ")
+
+                                                            // CHAPTER 03 (Variable For Number)
+
+// Q 1
+
+    // var age = ("I am 21 Year Old")
+    // alert(age)
+
+// Q 2
+
+//    var birthyear = ("My Birth Year is 2003")
+//    alert (birthyear)
+
+
+// Q 3
+
+//    var name = ("SAIN")
+//    alert (name)
+
+//    var  order =("5 T-Shirt(s)") 
+//    alert(order)
+
+//    var brand = ("J.")
+//    alert(brand)
